730cf9683275f5c4cfb4828046107386363d956d
truffo201 <mora_a@pc-administrateur.(none)>
2012-10-12 15:47:47 +0200

    Update main example.

--- main.cc
+++ main.cc
@@ -1,28 +1,32 @@
 #include "graphic.hh"
 #include "x11.hh"
+#include <string>
 
 int     main(int argc, char **argv)
 {
   Graphic *graph;
+  Windoo  *w;
   X11     *x11;
+  t_coord coord;
+  t_coord size;
 
   x11 = new X11();
   graph = static_cast<Graphic *>(x11);
   graph->load();
-  while (42)
-    graph->listenEvents();
-  graph->unload();
-  return 0;
-  /*
 
   coord.x = 0;
   coord.y = 0;
   size.x = 640;
   size.y = 480;
-  */
-  // create a new window
-  /*graph->windowCreate("Home", &coord, &size);
-  graph->windowShow("Home");
-  graph->windowSetTitle("Home", "KikooLoL");
-  */
+
+  graph->newWindoo(std::string("Home").c_str(), &coord, &size);
+  w = graph->getWindoo("Home");
+
+  while (42)
+  {
+    w->show();
+    graph->listenEvents();
+  }
+  graph->unload();
+  return 0;
 }
