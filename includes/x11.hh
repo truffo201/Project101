a31e6336f88383327caf02485e5fff3f26620873
truffo201 <mora_a@pc-administrateur.(none)>
2012-10-12 13:10:52 +0200

    Add newWindoo metho to X11 class.

--- includes/x11.hh
+++ includes/x11.hh
@@ -6,12 +6,11 @@
 #include "graphic.hh"
 #include "windoo.hh"
 #include <map>
+#include <string>
 
-/*
-typedef std::pair<std::map<const char *, t_window *>::iterator, bool> wpair;
-typedef std::map<const char *, t_window*> wmap;
-typedef std::map<const char *, t_window *>::iterator witerator;
-*/
+typedef std::map<const char *, Windoo *>::iterator witerator;
+typedef std::pair<std::map<const char *, Windoo *>::iterator, bool> wpair;
+typedef std::map<const char *, Windoo*> wmap;
 
 class X11 : public Graphic
 {
@@ -20,12 +19,13 @@ class X11 : public Graphic
     int  load();
     int  unload();
 
+  int newWindoo(std::string &name, t_coord *pos, t_coord *size);
     void listenEvents();
 
   private:
     Display     *_display;
     XEvent      _event;
-  //wmap        _windows;
+    wmap        _windows;
 
 };
 
--- x11.cc
+++ x11.cc
@@ -1,6 +1,11 @@
 #include "x11.hh"
+#include "wx11.hh"
+#include "windoo.hh"
 #include <X11/Xlib.h>
 #include <stdlib.h>
+#include <string>
+
+class Windoo;
 
 int X11::load()
 {
@@ -9,6 +14,20 @@ int X11::load()
   return 0; // success
 }
 
+int X11::newWindoo(std::string &name, t_coord *pos, t_coord *size)
+{
+  WX11   *wx11;
+  Windoo *w;
+  wpair  ret;
+
+  // create new Window
+  w = static_cast<Windoo *>(new WX11(name, pos, size, _display));
+  // save the window for use
+  ret = _windows.insert(std::pair<const char *, Windoo *>(name.c_str(), w));
+  if (ret.second == false)
+    return 2; // already exist
+  return 0; // success
+}
 int X11::unload()
 {
   // clean all windows from map before shutdown the connection
